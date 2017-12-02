10e76b79185d5d7bc803902e107a54e29a02f2e9
bimal <gist@bimal.org.np>
2017-12-02 03:31:24 -0700

    general

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>zygote scss</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<div class="logo">
+				<a href="#"><img src="images/logo.png" /></a>
+			</div>
+			<div class="nav">
+				<ul>
+					<li><a href="#">Home</a>
+					<li><a href="#">Reports</a>
+					<li><a href="#">Logout</a>
+				</ul>
+			</div>
+		</div>
+		<div class="main"></div>
+		<div class="footer"></div>
+	</div>
+</body>
+</html>
